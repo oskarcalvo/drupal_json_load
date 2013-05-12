f56e9384a7685c75bd8f23c1cfd9675d1d9fcb0a
oskar <calvo.oskar@gmail.com>
2013-05-12 15:58:18 +0200

    update the code with a more custom code

--- jsoncall.php
+++ jsoncall.php
@@ -10,7 +10,18 @@
  */
 define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-$node = node_load('1');
+//We only need the database options to works, son load it from bootstrap
+drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
 
-print_r(json_encode($node));
+//No we can use drupal db query  http://drupal.org/node/310075
+$query = db_select('users', 'u');
+$query->condition('u.uid', 0, '<>');
+$query->fields('u', array('uid', 'name', 'status', 'created', 'access'));
+$query->range(0, 50);
+$result = $query->execute();
+$record = $result->fetchObject();
+
+//echo "<br>";
+//print_r(microtime(true));
+print json_encode($record);
+exit;
